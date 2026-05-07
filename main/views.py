3f37abdbe4fcc558540913500158835b2afb3d2d
Karin <ykarinw@gmail.com>
2026-05-08 05:17:08 +0700

    Tut 2

--- main/views.py
+++ main/views.py
@@ -1,10 +1,62 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,  redirect, get_object_or_404
+from django.core import serializers
+from main.forms import NewsForm
+from main.models import News
 
 def show_main(request):
-    context = {
+   news_list = News.objects.all()
+   context = {
         'npm' : '240123456',
         'name': 'Haru Urara',
-        'class': 'PBP A'
+        'class': 'PBP A',
+        'news_list': news_list
+    }
+   
+   return render(request, "main.html", context)
+
+def create_news(request):
+    form = NewsForm(request.POST or None)
+
+    if form.is_valid() and request.method == "POST":
+        form.save()
+        return redirect('main:show_main')
+
+    context = {'form': form}
+    return render(request, "create_news.html", context)
+
+def show_news(request, id):
+    news = get_object_or_404(News, pk=id)
+    news.increment_views()
+
+    context = {
+        'news': news
     }
 
-    return render(request, "main.html", context)
+    return render(request, "news_detail.html", context)
+
+def show_xml(request):
+    news_list = News.objects.all()
+    xml_data = serializers.serialize("xml", news_list)
+    return HttpResponse(xml_data, content_type="application/xml")
+
+def show_json(request):
+    news_list = News.objects.all()
+    json_data = serializers.serialize("json", news_list)
+    return HttpResponse(json_data, content_type="application/json")
+
+def show_xml_by_id(request, news_id):
+    try:
+        news_item = News.objects.filter(pk=news_id)
+        xml_data = serializers.serialize("xml", news_item)
+        return HttpResponse(xml_data, content_type="application/xml")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
+    
+def show_json_by_id(request, news_id):
+    try:
+        news_item = News.objects.get(pk=news_id)
+        json_data = serializers.serialize("json", [news_item])
+        return HttpResponse(json_data, content_type="application/json")
+    except News.DoesNotExist:
+        return HttpResponse(status=404)
